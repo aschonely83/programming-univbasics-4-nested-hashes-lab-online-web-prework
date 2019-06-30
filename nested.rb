906340a6768b79030d808a396ae517c365918578
Adam Schonely <aschonely@gmail.com>
2019-06-30 01:22:26 +0000

    Automatically backed up by Learn

--- nested.rb
+++ nested.rb
@@ -15,7 +15,7 @@ def hopper
           :languages => ["C"]
         }
      }
-  puts programmer_hash[:grace_hopper][:known_for][:languages]
+   programmer_hash[:grace_hopper][:known_for][:languages]
 
 end
 
